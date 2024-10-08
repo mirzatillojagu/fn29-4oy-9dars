29f97ca767c5922ce2fe0cff69cb25cac94d4784
mirzatillojagu <imirzatillo0@gmail.com>
2024-10-08 16:50:24 +0500

    vazifa

---
+++ js/index.js
@@ -0,0 +1,185 @@
+/////////////////Oddiy masalalar////////////////////
+
+// 1
+// let arr = [1, 2, 3]
+
+// console.log(arr[0]);
+
+// console.log(arr[2]);
+
+// 2
+
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+
+// arr[4] = "salom"
+
+// console.log(arr);
+
+// 3
+// let arr = [];
+
+// arr.unshift('olma');
+// arr.unshift('anjir');
+// arr.unshift('shaftoli');
+// arr.unshift('nok');
+
+// console.log(arr);
+
+// 4
+// let arr = [1, 2, 3, 4]
+
+// console.log(arr.length);
+
+// 5
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.unshift('olma');
+
+// console.log(arr);
+
+// 6
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.pop();
+// console.log(arr);
+
+// 7
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.unshift('olma');
+
+// console.log(arr);
+
+// 8
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.shift();
+
+// console.log(arr);
+
+// 9
+// let arr = [1, 2, 3, 4, 5]
+
+// for(let i=0; i < arr.length; i++){
+//     console.log(arr[i]);
+
+// }
+
+// 10
+
+// let arr = [1,2,3,4,5,6]
+
+// let qiymat = 5;
+// if(arr.includes(qiymat)){
+//     console.log('Mavjud ');
+// }else{
+//     console.log('Mavjud emas');
+// }
+
+/////////////////////For loop ga oid masalalar///////////////////////
+
+// 1
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for(let i=0; i < arr.length; i++){
+//     console.log(arr[i]);
+
+// }
+
+// 2
+// let arr = ['olma', 'shaftoli', 'anjir', 'meva', 'meva']
+
+// for(let i=0; i < arr.length; i++){
+//     console.log(arr[i]);
+
+// }
+
+// 3
+// let arr = [1, 2, 3, 4, 5, 6]
+
+// for(let i =1; i < arr.length; i+=2){
+//     console.log(arr[i]);
+
+// }
+
+// 4
+// let arr = [1, 2, 3, 4, 5, 6]
+
+// for(let i =0; i < arr.length; i+=2){
+//     console.log(arr[i]);
+
+// }
+
+// 5
+// let arr = [1, 2, -3, 4, -5, -6]
+
+// for(let i=0; i < arr.length; i++){
+//     if (arr[i] > 0) {
+//         console.log(arr[i]);
+//     }
+// }
+
+// 6
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let res=arr.reverse()
+// console.log(res);
+
+// 7
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let sum=0;
+
+// for(let i=0; i < arr.length; i++){
+//     sum+=arr[i]
+// }
+
+// console.log(sum);
+
+// 8
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// let sum=[];
+
+// for(let i=0; i < arr.length; i++){
+//     sum.push(arr[i]*2)
+
+// }
+// console.log(sum);
+
+// 9
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let a = Math.max(...arr);
+
+// console.log(a);
+
+// 10
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let a = Math.min(...arr);
+
+// console.log(a);
+
+// 11
+// let arr = [1, 2, 3, 4, -5, 6, -7, 8, 9, -10];
+
+// for(let i=0; i < arr.length; i++){
+//     if (arr[i] < 0) {
+//         console.log(arr[i]);
+//     }
+// }
+
+// 12
+// let arr = [1, 2, 3, 4, -5, 6, -7, 8, 9, -10];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     sum += arr[i];
+//   }
+// }
+// console.log(sum);
+
+
